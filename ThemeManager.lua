1912908be646b735ff061cef6a13ea27af730a29
fugi <playerscenn@gmail.com>
2026-03-20 12:47:51 -0500

    Set default theme to 'Unnamed Enhancements'

--- ThemeManager.lua
+++ ThemeManager.lua
@@ -2,6 +2,7 @@ local httpService = game:GetService('HttpService')
 local ThemeManager = {} do
 	ThemeManager.Folder = 'LinoriaLibSettings'
 	ThemeManager.Library = nil
+	ThemeManager.DefaultTheme = 'Unnamed Enhancements'
 	ThemeManager.BuiltInThemes = {
 		['Default'] 		= { 1, httpService:JSONDecode('{"FontColor":"BFBFBF","MainColor":"0F0F0F","AccentColor":"929667","SelectedTabColor":"101010","BackgroundColor":"1B1B1B","OutlineColor":"0B0B0B"}') },
 		['GameSense'] 		= { 2, httpService:JSONDecode('{"FontColor":"919191","MainColor":"101010","AccentColor":"9CB819","SelectedTabColor":"101010","BackgroundColor":"111111","OutlineColor":"2D2D2D"}') },
@@ -9,6 +10,7 @@ local ThemeManager = {} do
 		['Tokyohook.cc'] 	= { 4, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"191925","AccentColor":"6759b3","SelectedTabColor":"1a1a29","BackgroundColor":"16161f","OutlineColor":"433e58"}') },
 		['pandahook.cc'] 	= { 5, httpService:JSONDecode('{"FontColor":"AEAEAE","MainColor":"0F0F0F","AccentColor":"30406A","SelectedTabColor":"151515","BackgroundColor":"0F0F0F","OutlineColor":"171717"}') },
 		['Mae.lua'] 	        = { 6, httpService:JSONDecode('{"FontColor":"c5c5c5","MainColor":"0F0F0F","AccentColor":"ffc6fe","SelectedTabColor":"171717","BackgroundColor":"0f0f0f","OutlineColor":"191919"}') },
+		['Unnamed Enhancements'] = { 7, httpService:JSONDecode('{"FontColor":"C3C8D7","MainColor":"12141E","AccentColor":"586EC3","SelectedTabColor":"161A28","BackgroundColor":"0D0F17","OutlineColor":"1E2234"}') },
 	}
 
 	function ThemeManager:ApplyTheme(theme)
@@ -46,7 +48,7 @@ local ThemeManager = {} do
 	end
 
 	function ThemeManager:LoadDefault()		
-		local theme = 'Tokyohook.cc'
+		local theme = 'Unnamed Enhancements'
 		local content = isfile(self.Folder .. '/themes/default.txt') and readfile(self.Folder .. '/themes/default.txt')
 
 		local isDefault = true
